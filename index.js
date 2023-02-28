ac0fda621e90c7037cdc9cfc0518fe5698eabfcb
shinesonice <shinenightlife@gmail.com>
2023-02-28 15:02:59 +0700

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+const POST = process.env.POST || 5050;
+
+app.use("/", req, res, (next) => {
+  console.log("oke");
+});
+
+app.listen(POST);
